84a22a0fdff5ec56231370f593b627b2b75c6325
gvkiran007 <gvkiran007@gamil.com>
2019-08-01 20:14:07 +0530

    Add project files.

---
+++ kSoft.Core/kSoft.Core.Android/SplashActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace kSoft.Core.Droid
+{
+    [Activity(Label = "kSoft.Core", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/SplashTheme", 
+        MainLauncher = true, 
+        NoHistory =true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            CallMainActivity();           
+        }
+
+       private void CallMainActivity()
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+        }
+    }
+}
