3cdad2dcd5e12a87f78bda0118e20a230003bc00
gvkiran007 <gvkiran007@gamil.com>
2019-08-05 20:17:09 +0530

    added Generic Repo

---
+++ kSoft.Core/kSoft.Core/Contracts/Repository/IKSoftRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kSoft.Core.Contracts.Repository
+{
+    public interface IKSoftRepository
+    {
+        Task<T> GetAsync<T>(string uri, string authToken = null);
+        Task<T> PostAsync<T>(string uri, T data, string authToken = null);
+        Task<T> PutAsync<T>(string uri, T data, string authToken = null);
+        Task DeleteAsync(string uri, string authToken = null);
+        Task<R> PostAsync<T, R>(string uri, T data, string authToken = null);
+    }
+}
---
+++ kSoft.Core/kSoft.Core/Repository/KSoftRepository.cs
@@ -0,0 +1,82 @@
+﻿using kSoft.Core.Contracts.Repository;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kSoft.Core.Repository
+{
+    public class KSoftRepository : IKSoftRepository
+    {
+        public Task DeleteAsync(string uri, string authToken = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetAsync<T>(string uri, string authToken = null)
+        {
+            try
+            {
+                string jsonResult = string.Empty;
+                using (HttpClient httpClient = CreateHttpClient(authToken))
+                {
+                    var response =  await httpClient.GetAsync(uri);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        jsonResult = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                        var json = JsonConvert.DeserializeObject<T>(jsonResult);
+                        return json;
+                    }
+
+                    if (response.StatusCode == HttpStatusCode.Forbidden ||
+                        response.StatusCode == HttpStatusCode.Unauthorized)
+                    {
+                        //throw new ServiceAuthenticationException(jsonResult);
+                    }
+
+                   // throw new HttpRequestExceptionEx(responseMessage.StatusCode, jsonResult);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public Task<T> PostAsync<T>(string uri, T data, string authToken = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<R> PostAsync<T, R>(string uri, T data, string authToken = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> PutAsync<T>(string uri, T data, string authToken = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        private HttpClient CreateHttpClient(string authToken)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            if (!string.IsNullOrEmpty(authToken))
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
+            }
+
+            return httpClient;
+        }
+    }
+}
